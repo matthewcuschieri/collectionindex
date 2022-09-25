18e8cac087fdc60ebe118e1e9652bfc71fb761b8
mattcoosh <mcuschie@risd.edu>
2022-09-24 22:28:29 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,32 @@
+$(function () {
+  $("#rowone").on("click", function () {
+    $("#contentone").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    $("#spacing").toggle();
+  });
+});
+$(function () {
+  $("#rowtwo").on("click", function () {
+    $("#contenttwo").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    $("#spacing").toggle();
+  });
+});
+$(function () {
+  $("#rowthree").on("click", function () {
+    $("#contentthree").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    $("#spacing").toggle();
+  });
+});
+$(function () {
+  $("#rowfour").on("click", function () {
+    $("#contentfour").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    $("#spacing").toggle();
+  });
+});

370ce2cdde9d8bc2bdfb6fc7527531ef127996de
mattcoosh <mcuschie@risd.edu>
2022-09-26 17:42:43 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -30,3 +30,30 @@ $(function () {
     $("#spacing").toggle();
   });
 });
+$(function () {
+  $("#catone").on("click", function () {
+    $("#popup").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    var videoFile =
+      "//dl.dropbox.com/s/931244iox7i0fpk/working-with-espresso.mp4";
+    var video = $("#change video")[0];
+    video.src = videoFile;
+    video.load();
+    video.play();
+  });
+});
+
+$(function () {
+  $("#cattwo").on("click", function () {
+    $("#popup").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    var videoFile =
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+    var video = $("#change video")[0];
+    video.src = videoFile;
+    video.load();
+    video.play();
+  });
+});

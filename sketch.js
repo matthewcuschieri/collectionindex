958971c5efe336299187813476982591861a830c
mattcoosh <mcuschie@risd.edu>
2022-09-30 17:33:28 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ $(function () {
   });
 });
 $(function () {
-  $("#rowtwo").on("click", function () {
+  $("#wordtwo").on("click", function () {
     $("#contenttwo").toggle();
     // $("#content").toggle();
     // $("#c").toggle();
@@ -15,7 +15,7 @@ $(function () {
   });
 });
 $(function () {
-  $("#rowthree").on("click", function () {
+  $("#wordthree").on("click", function () {
     $("#contentthree").toggle();
     // $("#content").toggle();
     // $("#c").toggle();
@@ -23,8 +23,40 @@ $(function () {
   });
 });
 $(function () {
-  $("#rowfour").on("click", function () {
+  $("#wordfour").on("click", function () {
     $("#contentfour").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    $("#spacing").toggle();
+  });
+});
+$(function () {
+  $("#wordfive").on("click", function () {
+    $("#contentfive").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    $("#spacing").toggle();
+  });
+});
+$(function () {
+  $("#wordsix").on("click", function () {
+    $("#contentsix").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    $("#spacing").toggle();
+  });
+});
+$(function () {
+  $("#wordseven").on("click", function () {
+    $("#contentseven").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    $("#spacing").toggle();
+  });
+});
+$(function () {
+  $("#wordeight").on("click", function () {
+    $("#contenteight").toggle();
     // $("#content").toggle();
     // $("#c").toggle();
     $("#spacing").toggle();
@@ -46,6 +78,32 @@ $(function () {
 
 $(function () {
   $("#cattwo").on("click", function () {
+    $("#popup").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    var videoFile =
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4";
+    var video = $("#change video")[0];
+    video.src = videoFile;
+    video.load();
+    video.play();
+  });
+});
+$(function () {
+  $("#catthree").on("click", function () {
+    $("#popup").toggle();
+    // $("#content").toggle();
+    // $("#c").toggle();
+    var videoFile =
+      "//dl.dropbox.com/s/931244iox7i0fpk/working-with-espresso.mp4";
+    var video = $("#change video")[0];
+    video.src = videoFile;
+    video.load();
+    video.play();
+  });
+});
+$(function () {
+  $("#catfour").on("click", function () {
     $("#popup").toggle();
     // $("#content").toggle();
     // $("#c").toggle();
